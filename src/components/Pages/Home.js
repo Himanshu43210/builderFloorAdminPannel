b2898373074851bb80733a9aec905242719be5d1
akhilTechHelps <132357452+akhilTechHelps@users.noreply.github.com>
2023-07-25 16:26:02 +0530

    Update Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,10 +3,13 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { HOME_SCREEN } from "../../ScreenJson";
 import RenderComponent from "../customComponents/ComponentRenderer";
+import MenupState from "../customComponents/MenupState";
+import { MenuItems } from "../../ScreenJson";
 
 export default function Home() {
   return (
     <>
+      <MenupState MenuItems={MenuItems} />
       <Card className="home-screen">
         <RenderComponent jsonToRender={HOME_SCREEN} />
       </Card>
