239c1635e3ed345aef4552ef78fd8716f9be9dbc
techhelpskeven <135963987+techhelpskevin@users.noreply.github.com>
2023-08-02 13:08:35 +0530

    css

--- src/components/customComponents/Footer.jsx
+++ src/components/customComponents/Footer.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { LINKEDIN_ICON } from '../utils/Const';
+import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { LINKEDIN_ICON } from "../utils/Const";
 
 export default function Footer({ component }) {
-    
-    return (
-        <footer>
-            <div>
-                <a className='footerlink' href={component.HomeLinks.url}><img src="https://www.builderfloor.com/assets/imgs/template/BUILDER.png" alt="" width='40px' height='50px' /></a>
-            </div>
-            <hr />
-            <hr />
-            <div >
-                {component.social_media.map((social, index) => {
-                    const SocialIcon = social.name === LINKEDIN_ICON ? LinkedInIcon : InstagramIcon;
-                    return(
-
-                    <a key={index} href={social.url}>
-                        <SocialIcon />
-                    </a>
-                )})}
-                
-            </div>
-            <div >
-                <p className='copyright'>{component.copyright}</p>
-            </div>
-        </footer>
-    );
-};
-
+  return (
+    <footer className="footerdiv">
+      <div className="footerlogo">
+        <a className="footerlink" href={component.HomeLinks.url}>
+          <img
+            src="https://www.builderfloor.com/assets/imgs/template/BUILDER.png"
+            alt=""
+            width="40px"
+            height="50px"
+          />
+        </a>
+      </div>
+      <hr />
+      <hr />
+      <div className="footer-social-icon">
+        {component.social_media.map((social, index) => {
+          const SocialIcon =
+            social.name === LINKEDIN_ICON ? LinkedInIcon : InstagramIcon;
+          return (
+            <a key={index} href={social.url}>
+              <SocialIcon />
+            </a>
+          );
+        })}
+      </div>
+      <div className="footer-copyright">
+        <p className="copyright">{component.copyright}</p>
+      </div>
+    </footer>
+  );
+}
