6ab6e469fb81038e1928fba7e5ab2348e2a98ee1
unknown <partharora557@gmail.com>
2023-09-26 09:05:35 +0530

    class changes

--- src/components/utils/Panel.js
+++ src/components/utils/Panel.js
@@ -14,72 +14,74 @@ import { resetApiStatus } from "../../redux/slice/apiSlice";
 function Panel({ nonSalesUser, handlePageClick, onLogoutClick }) {
   const dispatch = useDispatch();
   return (
-    <div>
-      <div className="sidebarcontainer">
-        <div className="panel-nav-link" id="sidebar">
-          {/* <Link to="/" className="panel-link">
+    <>
+      {/* <div>
+      <div className="sidebarcontainer"> */}
+      <div className="panel-nav-link" id="sidebar">
+        {/* <Link to="/" className="panel-link">
             <AiFillHome className="admin-panel-icons" /> &nbsp;
             <h6>BuilderFloor</h6>
           </Link> */}
-          <a
-            href="https://www.builderfloor.com"
-            className="panel-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AiFillHome className="admin-panel-icons" /> &nbsp;
-            <h6>BuilderFloor</h6>
-          </a>
+        <a
+          href="https://www.builderfloor.com"
+          className="panel-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillHome className="admin-panel-icons" /> &nbsp;
+          <h6>BuilderFloor</h6>
+        </a>
 
-          <Button
-            onClick={() => handlePageClick(ADMIN_DASHBOARD)}
-            className="panel-link"
-          >
-            <FaClipboardList className="admin-panel-icons" /> &nbsp;
-            <h6>Dashboard</h6>
-          </Button>
-          <>
-            {nonSalesUser && (
-              <>
-                <Button
-                  onClick={() => handlePageClick(USER_MANAGEMENT)}
-                  className="panel-link"
-                >
-                  <FaUsers className="admin-panel-icons" />
-                  <h6>User</h6>
-                </Button>
+        <Button
+          onClick={() => handlePageClick(ADMIN_DASHBOARD)}
+          className="panel-link"
+        >
+          <FaClipboardList className="admin-panel-icons" /> &nbsp;
+          <h6>Dashboard</h6>
+        </Button>
+        <>
+          {nonSalesUser && (
+            <>
+              <Button
+                onClick={() => handlePageClick(USER_MANAGEMENT)}
+                className="panel-link"
+              >
+                <FaUsers className="admin-panel-icons" />
+                <h6>User</h6>
+              </Button>
 
-                {/* <Button
+              {/* <Button
                   onClick={() => handlePageClick(MASTER_MANAGEMENT)}
                   className="panel-link"
                 >
                   <FaUserShield className="admin-panel-icons" />
                   <h6>Master</h6>
                 </Button> */}
-              </>
-            )}
-          </>
-          <Button
-            onClick={() => handlePageClick(PROPERTY_MANAGEMENT)}
-            className="panel-link"
-          >
-            <FaCog className="admin-panel-icons" />
-            <h6>Property</h6>
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(resetApiStatus(onLogoutClick));
-              localStorage.removeItem("email");
-              localStorage.removeItem("login");
-            }}
-            className="panel-link"
-          >
-            <AiOutlineLogout className="admin-panel-icons" />
-            <h6>Logout</h6>
-          </Button>
-        </div>
+            </>
+          )}
+        </>
+        <Button
+          onClick={() => handlePageClick(PROPERTY_MANAGEMENT)}
+          className="panel-link"
+        >
+          <FaCog className="admin-panel-icons" />
+          <h6>Property</h6>
+        </Button>
+        <Button
+          onClick={() => {
+            dispatch(resetApiStatus(onLogoutClick));
+            localStorage.removeItem("email");
+            localStorage.removeItem("login");
+          }}
+          className="panel-link"
+        >
+          <AiOutlineLogout className="admin-panel-icons" />
+          <h6>Logout</h6>
+        </Button>
       </div>
-    </div>
+      {/* </div>
+    </div> */}
+    </>
   );
 }
 
